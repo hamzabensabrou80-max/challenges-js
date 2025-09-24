686b4ba7f80574d11f36287a7d6c4cfef444974c
hamza bensabrou <HAMZABENSABROU80@gmail.com>
2025-09-24 16:48:39 +0100

    exercices

---
+++ test.js
@@ -0,0 +1,8 @@
+ let nombre1 = prompt("entrer 1 number");
+ console.log(nombre1);
+ let nombre2 = prompt("entrer 2 number");
+console.log(nomber2);
+let nombre3 = prompt("entrer3 number");
+console .log(nombre3);
+let nombre4 = prompt ("entrer");
+console.log(nombre4)
